735ebf5ddb968cf3f4198b166a06434d1c3fe112
Sean Nakagawa <nakagawasean@gmail.com>
2020-05-26 11:32:52 +0200

    Upload w3d5

--- W3D5/script.js
+++ W3D5/script.js
@@ -1,5 +1,3 @@
-// Function Task Items (Create, Create Text, Create Checkbox, Delete Button)
-
 const createTaskItem = (task) => {
     const div = document.createElement('div');
     div.classList.add('task-item');
@@ -10,15 +8,6 @@ const createTaskItem = (task) => {
     const tasktext = document.createTextNode(task.text);
     p.appendChild(tasktext);
     div.appendChild(p);
-    if (task.done) {
-        checkbox.checked = true;
-        p.classList.add('strikethrough');
-    }
-
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.classList.add('checkbox-done');
-    div.appendChild(checkbox);
 
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-button');
@@ -27,19 +16,13 @@ const createTaskItem = (task) => {
 }
 
 
-
-// Eventlistener on Click
-
-const taskContainer = document.querySelector('main');
+const taskContainer = document.querySelector('content');
 taskContainer.addEventListener('click', () => {
     handleEventListener(event)
 });
 
 
-
-// Add Tasks to DOM (Nav)
-
-const addTasksToNav = async () => {
+const addTasksToMain = async () => {
     const tasks = await convertResponse();
     const taskItems = tasks.map(item => {
         const taskItem = createTaskItem(item);
@@ -50,6 +33,7 @@ const addTasksToNav = async () => {
     });
 }
 
+
 const convertResponse = async () => {
     const result = await getAllTasks();
     let tasks = Object.keys(result).map(key => ({
@@ -59,6 +43,7 @@ const convertResponse = async () => {
     }));
     return tasks;
 }
+
 
 const handleNavInput = async () => {
     const taskText = navTextInput.value;
@@ -79,60 +64,12 @@ const handleEventListener = async (event) => {
         const hash = event.target.closest('.task-item').id;
         await deleteTask(hash);
         event.target.closest('.task-item').remove();
-    } else if (event.target.matches('.checkbox-done')) {
-        const hash = event.target.closest('.task-item').id;
-        const text = event.target.closest('.task-item').children[1].textContent;
-        const done = event.target.closest('.checkbox-done').checked ? true : false;
-        if (done) {
-            event.target.closest('.task-item').children[1].classList.add('strikethrough');
-        } else {
-            event.target.closest('.task-item').children[1].classList.remove('strikethrough');
-        }
-        const newTask = {
-            text: text,
-            done: done
-        };
-        await putTask(hash, newTask);
-    } else if (event.target.matches('.task-text')) {
-
-        const button = document.createElement('button');
-        button.classList.add('save-button');
-        const text = document.createTextNode('Save');
-        button.appendChild(text);
-        newInput.parentNode.insertBefore(button, newInput.nextSibling);
-
-        const newInput = document.createElement('input');
-        newInput.type = "text";
-        newInput.classList.add('edit-text');
-        newInput.value = event.target.textContent;
-        event.target.closest('.task-item').replaceChild(newInput, event.target);
-
-
-
-        newInput.focus();
-    } else if (event.target.matches('.save-button')) {
-
-        const p = document.createElement('p');
-        const textInput = event.target.closest('.task-item').children[1];
-        p.classList.add('task-text');
-
-        const text = document.createTextNode(textInput.value);
-        const hash = event.target.closest('.task-item').id;
-        const done = textInput.closest('.task-item').children[0].checked ? true : false;
-        const updatedTask = {
-            text: text.textContent,
-            done: done
-        };
-        const x = await putTask(hash, updatedTask);
-
-        p.appendChild(text);
-        event.target.closest('.task-item').replaceChild(p, textInput);
-        event.target.remove();
-    }
+    } 
 }
+
 
 const navTextInput = document.querySelector('nav input');
 const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', handleNavInput)
 
-addTasksToNav();
+addTasksToMain();
